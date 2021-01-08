9b06865812f7bbacfd1685100e649aab16bcbb4f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-01-08 10:14:51 +0100

    NO celery in servicelib

--- packages/service-library/src/servicelib/logging_utils.py
+++ packages/service-library/src/servicelib/logging_utils.py
@@ -9,8 +9,6 @@ from asyncio import iscoroutinefunction
 from inspect import getframeinfo, stack
 from typing import Callable, Dict, Optional
 
-from celery.app.log import Logging
-
 log = logging.getLogger(__name__)
 
 
@@ -76,7 +74,7 @@ def config_all_loggers():
 
 def set_logging_handler(
     logger: logging.Logger,
-    formatter_base: Optional[Logging] = None,
+    formatter_base=None,
     formatting: Optional[str] = None,
 ) -> None:
     if not formatting:
